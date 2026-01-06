5012472499fd27f3fa99dc576b4a4f57e0b8850c
Mohammed Mahdi <mo7ammed.salah@outlook.com>
2026-01-07 01:34:40 +0300

    chore(api): add `ProjectsController` with endpoints for managing projects

---
+++ Nexus.Api/Controllers/Projects/ProjectsController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Nexus.Core.Interfaces.Projects;
+using Nexus.Core.DTOs.Projects;
+
+namespace Nexus.Api.Controllers.Projects;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController(IProjectService projectService) : ControllerBase
+{
+    /// <summary>
+    /// Get all projects
+    /// </summary>
+    // GET: api/projects
+    [HttpGet]
+    public async Task<ActionResult<List<ProjectResponse>>> GetAll()
+    {
+        var projects = await projectService.GetAllAsync();
+        return Ok(projects);
+    }
+
+    ///<summary>
+    /// Get project by Id
+    /// </summary>
+    // GET: api/projects/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProjectResponse?>> GetById(int id)
+    {
+        var project = await projectService.GetByIdAsync(id);
+        if(project == null) return NotFound();
+        
+        return Ok(project);
+    }
+
+    /// <summary>
+    /// Create a new project
+    /// </summary>
+    // POST: api/projects
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProjectRequest request)
+    {
+        var project = await projectService.CreateAsync(request);
+
+        return CreatedAtAction(nameof(GetById), new { id = project.Id }, project);
+    }
+}
